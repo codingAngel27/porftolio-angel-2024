c5f912dc4895416912a5b4f1cdc86560a573a797
Ruben <angelocana44@gmail.com>
2024-09-25 00:58:51 -0500

    cambios finales

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				customPurple: '#17153B',
+				customPurple2: '#0C1844',  // Aquí defines el color personalizado
+			  },
+		},
 	},
 	plugins: [],
 }
